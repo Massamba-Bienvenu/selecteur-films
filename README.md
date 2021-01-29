0ac887b9276a2c7b1ec63cfc72a3531556afb6f6
Bienvenu Massamba <boukamass@gmail.com>
2021-01-29 06:19:34 -0600

    rendered pagination

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## APP WEBSITE
 
 This application website is available at the following adress" https://Massamba-Bienvenu.github.io/selecteur-films
+
+# PAGINATION
+
+The lodash npm package was used to implement the pagination component.
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
+import Pagination from "../components/Pagination";
 import { getMovies } from "../services/fakeMovieService.js";
 
 export class Movies extends Component {
   state = {
+    pageSize: 4,
     movies: getMovies(),
   };
 
@@ -11,37 +13,53 @@ export class Movies extends Component {
     this.setState({ movies: movies });
   };
 
+  handlePageChange = (page) => {
+    console.log(page);
+  };
+
   render() {
+    const message =
+      this.state.movies.length === 0
+        ? `There are no movies in the database.`
+        : `Showing ${this.state.movies.length} movies in the database`;
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Genre</th>
-            <th>Stock</th>
-            <th>Rate</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.movies.map((movie) => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td>
-                <button
-                  onClick={() => this.handleDelete(movie)}
-                  className="btn btn-danger btn-sm"
-                >
-                  Delete
-                </button>
-              </td>
+      <React.Fragment>
+        <p>{message}</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <Pagination
+          itemsCount={this.state.movies.length}
+          pageSize={this.state.pageSize}
+          onPageChange={this.handlePageChange}
+        />
+      </React.Fragment>
     );
   }
 }
